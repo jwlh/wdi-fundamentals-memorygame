9c91b24f0ed22c1a4823e4e0d17bf9e9ecfcdf23
jonathan hall <jonnyhall@jonathans-MacBook-Pro-3.local>
2017-09-06 15:08:56 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen" , "queen" , "king" , "king"]
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queeen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = []
 var checkForMatch = function(){
 		if (cardsInPlay.length === 2) {
@@ -10,10 +31,13 @@ var checkForMatch = function(){
 	};
 }
 var flipCard = function(cardID){
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 	checkForMatch();
 };
+
 
 flipCard(0);
 flipCard(2);
