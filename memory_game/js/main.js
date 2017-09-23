770fddd8ab83173601b44c150e7556d89e411574
jonathan hall <jonnyhall@jonathans-mbp-3.home>
2017-09-23 09:22:20 +0100

    shuffled cards, added more cards & cleared cardsInPlay after each turn

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,3 @@
-
 var cards = [
 {
 	rank: "queen",
@@ -17,40 +16,39 @@ var cards = [
 },
 {
 	rank: "king",
-	suit: "hearts",
+	suit: "diamonds",
 	cardImage: "images/king-of-diamonds.png"
+},
+{
+	rank: "queen",
+	suit: "clubs",
+	cardImage: "images/queen-of-clubs.png"
+},
+{
+	rank: "queen",
+	suit: "spades",
+	cardImage: "images/queen-of-spades.png"
+},
+{
+	rank: "king",
+	suit: "clubs",
+	cardImage: "images/king-of-clubs.png"
+},
+{
+	rank: "king",
+	suit: "spades",
+	cardImage: "images/king-of-spades.png"
 }
 ];
 
+// shuffle cards
+cards.sort(function(a, b){return 0.5 - Math.random()});
 
 //array to store cards in play
 var cardsInPlay = [];
 
-
-//function to check for match
-var checkForMatch = function() {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-};
-
-
-//function to flip a card
-var flipCard = function() {
-	cardId = this.getAttribute('data-id');
-	cardsInPlay.push(cards[cardId].rank);
-	this.setAttribute('src', cards[cardId].cardImage);
-
-	if (cardsInPlay.length === 2) {
-	checkForMatch();
-	}
-};
-
-
 //create board
-var createBoard = function() {
+function createBoard() {
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
@@ -60,22 +58,32 @@ var createBoard = function() {
 	}
 }
 
+//function to check for match
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+	cardsInPlay.length = 0;
+};
+
+//function to flip a card
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2) {
+	checkForMatch();
+	}
+};
+
 createBoard();
 
-
 // reset button
-
-
-
-var resetGame = function() {
+function resetGame() {
 	location.reload();
 }
 
 document.getElementById('reset').addEventListener('click', resetGame)
-
-
-
-
-
-
-
